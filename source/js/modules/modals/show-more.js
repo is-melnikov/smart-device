ca3e19d64da71bd964cf3b54465a6e64866a0df2
Ivan Melnikov <klubauto.vk@gmail.com>
2022-11-02 17:22:53 +0300

    Исправляет ошибки: блок about

--- source/js/modules/modals/show-more.js
+++ source/js/modules/modals/show-more.js
@@ -1,24 +1,20 @@
 const ABOUT_BUTTON = document.querySelector('.about__button');
-const ABOUT_DESCRIPTIONS = document.querySelectorAll('.about__description');
+const ABOUT_DESCRIPTION = document.querySelector('.about__description');
 
 const showMore = () => {
   ABOUT_BUTTON.classList.remove('about__button--nojs');
 
-  ABOUT_DESCRIPTIONS.forEach((aboutDescription) => {
-    aboutDescription.classList.remove('about__description--nojs');
-    aboutDescription.classList.add('about__description--hidden');
-  });
+  ABOUT_DESCRIPTION.classList.remove('about__description--nojs');
+  ABOUT_DESCRIPTION.classList.add('about__description--hidden');
 
   ABOUT_BUTTON.addEventListener('click', function () {
-    ABOUT_DESCRIPTIONS.forEach((aboutDescription) => {
-      if (aboutDescription.classList.contains('about__description--hidden')) {
-        aboutDescription.classList.remove('about__description--hidden');
-        ABOUT_BUTTON.textContent = 'Свернуть';
-      } else {
-        aboutDescription.classList.add('about__description--hidden');
-        ABOUT_BUTTON.textContent = 'Подробнее';
-      }
-    });
+    if (ABOUT_DESCRIPTION.classList.contains('about__description--hidden')) {
+      ABOUT_DESCRIPTION.classList.remove('about__description--hidden');
+      ABOUT_BUTTON.textContent = 'Свернуть';
+    } else {
+      ABOUT_DESCRIPTION.classList.add('about__description--hidden');
+      ABOUT_BUTTON.textContent = 'Подробнее';
+    }
   });
 };
 
